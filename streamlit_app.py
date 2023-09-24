de7f2962d0e5ba2d5b41631ed336f94d9a6ee87a
ErnestoChulo <142848942+ErnestoChulo@users.noreply.github.com>
2023-09-23 18:10:58 -0600

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,5 @@ st.header("Comienza aqui")
 st.text("Este chat te ayudara a revisar documentos PDF para sacar lo mas relevante de ellos")
 st.markdown(" > # ** Hola mundo**")#negritas, tamaño
 st.markdown("---")
-st.markdown("> Bordo")
+st.markdown("> Bordo") #linea horizontal
+st.markdown("[Dame click] (https://www.youtube.com/watch?v=dQw4w9WgXcQ)")

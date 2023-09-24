4a1d76d6c7d084951e5f41baddc601e720a6bfcf
ErnestoChulo <142848942+ErnestoChulo@users.noreply.github.com>
2023-09-23 18:00:46 -0600

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 #hola 
 import streamlit as st
-st.title("Hola como estas")
+st.title("CHATBOT DOCUMENTOS")
+st.subheader("TU AMIGO CHAT")
+st.header("Comienza aqui")
+st.text("Este chat te ayudara a revisar documentos PDF para sacar lo mas relevante de ellos")
+st.markdown(" > **#Hola mundo**")#negritas
